72a90aacd70327754b83158ecd600870ced44aac
hijuliancode <hijuliansosa@protonmail.com>
2020-11-28 17:28:40 -0500

    section-02: Introducción a los Tipos de Datos

---
+++ typescript/lessons/Introducción a los Tipos de Datos.ts
@@ -0,0 +1,19 @@
+(function() {
+
+  let mensaje: string = "hola";
+  let numero: number = 123;
+  let booleano: boolean = true;
+  let hoy: Date = new Date();
+
+  let cualquierCosa: any;
+  cualquierCosa = mensaje;
+  cualquierCosa = numero;
+  cualquierCosa = booleano;
+  cualquierCosa = hoy;
+
+  let spiderman = {
+    nombre: 'Peter',
+    edad: 30
+  }
+
+})();
