6282fa9ebd94b9242d57874364f361b3489b518c
hijuliancode <hijuliansosa@protonmail.com>
2020-11-28 20:17:14 -0500

    section-02: Promesas 2

---
+++ typescript/lessons/promesas-201.ts
@@ -0,0 +1,22 @@
+(() => {
+  console.log('Start');
+
+    const retirarDinero = (montoRetirar: number): Promise<number> => {
+      let dineroActual = 1000;
+
+      return new Promise((resolve, reject) => {
+        if (montoRetirar > dineroActual) {
+          reject(`No hay suficientes fondos, quedan: ${dineroActual}`)
+        } else {
+          dineroActual -= montoRetirar;
+          resolve(dineroActual)
+        }
+      })
+    }
+
+    retirarDinero(500)
+      .then( montoActual => console.log(`Me queda ${montoActual}`) )
+      .catch( console.warn );
+
+  console.log('End');
+})();
