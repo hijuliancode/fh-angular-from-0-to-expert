3e051e3db5b67a6d994fff9e280e30c200d90564
hijuliancode <hijuliansosa@protonmail.com>
2020-11-28 21:36:40 -0500

    section-02: Clases

---
+++ typescript/lessons/clases.ts
@@ -0,0 +1,30 @@
+(() => {
+  // class Avenger {
+  //   nombre: string;
+  //   equipo: string;
+  //   nombreReal: string;
+
+  //   puedePelear: boolean;
+  //   peleasGanadas: number;
+
+  //   constructor(nombre: string, equipo: string) {
+  //     this.nombre = nombre;
+  //     this.equipo = equipo;
+  //   }
+  // }
+
+  class Avenger {
+    constructor(
+      public nombre: string,
+      public equipo: string,
+      public nombreReal?: string,
+      public puedePelear: boolean = true,
+      public peleasGanadas: number = 0,
+    ) {
+    }
+  }
+
+  const antman: Avenger = new Avenger('Antman', 'Capi', undefined, undefined, 8 );
+
+  console.log(antman)
+})();
