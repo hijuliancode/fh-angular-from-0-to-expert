c56e42af542b77160c3226989e49b820b1e5a933
hijuliancode <hijuliansosa@protonmail.com>
2020-11-29 22:33:39 -0500

    section-03: TemplateUrl: Separando el HTML el componente

--- 03-hello-world-app/src/app/components/header/header.component.ts
+++ 03-hello-world-app/src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h2>Header Component</h2>`
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
