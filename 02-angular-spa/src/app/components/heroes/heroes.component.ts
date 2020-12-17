776af9c5a48df58ac8a569e755573638595d0167
hijuliancode <hijuliansosa@protonmail.com>
2020-12-16 23:52:20 -0500

    section-04: Rutas con parametros - Router

--- 02-angular-spa/src/app/components/heroes/heroes.component.ts
+++ 02-angular-spa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Heroe } from '../../types/heroe.interface';
 
@@ -13,12 +14,17 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = []
 
   constructor(
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.heroes = this.heroesService.getHeroes()
     console.log(this.heroes)
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe', idx])
   }
 
 }
